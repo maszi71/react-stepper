3e8480c03dcfdbe6f53309309c904ef2b62d6a5a
masoudvrz <masoud.varzi@yahoo.com>
2022-03-03 19:53:41 +0330

    install react-tooltip

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -2,40 +2,46 @@ import { useState } from "react";
 import classes from "./Stepper.module.scss";
 import StepTitle from "./StepTitle/StepTitle";
 import Card from "./Card/Card";
+import FirstStep from "./Steps/FirstStep/FirstStep";
+import SecondStep from "./Steps/SecondStep/SecondStep";
 
 const Stepper = () => {
-    const [activeIndex, setActiveIndex] = useState(1);
-    const UpdateActiveStep = (index) => {
-        setActiveIndex(index);
-      };
+  const [activeIndex, setActiveIndex] = useState(1);
+  const [generalInfo, setGeneralInfo] = useState(null);
+
+  const UpdateActiveStep = (index) => {
+    setActiveIndex(index);
+  };
+
+  const getGeneralInfo = (info) => {
+    setGeneralInfo(info);
+  };
   return (
     <section className={classes.section}>
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-xl-9 col-lg-11 col-12 d-md-block d-none">
             <Card className={classes["title-card"]}>
-              <StepTitle
-                index="1"
-                activeIndex={activeIndex}
-                title="Step1"
-              />
+              <StepTitle index="1" activeIndex={activeIndex} title="Step1" />
               <div
                 style={{ background: activeIndex > 1 ? "#204B90" : "#C4C4C4" }}
                 className={classes.line}
               ></div>
-              <StepTitle
-                index="2"
-                activeIndex={activeIndex}
-                title="Step2"
-              />
+              <StepTitle index="2" activeIndex={activeIndex} title="Step2" />
               <div
                 style={{ background: activeIndex > 2 ? "#204B90" : "#C4C4C4" }}
                 className={classes.line}
               ></div>
-              <StepTitle
-                index="3"
+              <StepTitle index="3" activeIndex={activeIndex} title="Step3" />
+            </Card>
+          </div>
+          <div className="col-xl-9 col-lg-11 col-12">
+            <Card className={classes["step-card"]}>
+              <FirstStep
+                OnUpdateActiveStep={UpdateActiveStep}
+                onGetGeneralInfo={getGeneralInfo}
+                generalInfo={generalInfo}
                 activeIndex={activeIndex}
-                title="Step3"
               />
             </Card>
           </div>
--- src/components/Steps/firstStep/FirstStep.js
+++ src/components/Steps/firstStep/FirstStep.js
@@ -1,7 +1,87 @@
 import classes from "./FirstStep.module.scss";
+import { useForm } from "react-hook-form";
+import SimpleInput from "../../SimpleInput/SimpleInput";
 
-const FirstStep = ()=> {
-    return(<div></div>)
-}
+const FirstStep = ({
+  OnUpdateActiveStep,
+  onGetGeneralInfo,
+  generalInfo,
+  activeIndex,
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    getValues,
+  } = useForm({
+    defaultValues: generalInfo
+      ? {
+          companyName: generalInfo["companyName"],
+          companyIntroduction: generalInfo["companyIntroduction"],
+          serviceDescription: generalInfo["serviceDescription"],
+          companySize: generalInfo["companySize"],
+          lifeScienceSector: generalInfo["lifeScienceSector"],
+          profilePicture: generalInfo["profilePicture"],
+          attachFiles: generalInfo["attachFiles"],
+        }
+      : {},
+  });
+
+  const onSubmit = (submittedForm) => {
+    console.log("generalInfo", submittedForm);
+    onGetGeneralInfo(submittedForm);
+    OnUpdateActiveStep(2);
+  };
+
+  return (
+    <form
+      style={{ display: activeIndex === 1 ? "block" : "none" }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="row">
+        <div className="col-md-6 col-12">
+          <SimpleInput
+            validation={{
+              required: true,
+            }}
+            input={{
+              type: "text",
+              label: "First Name",
+              name: "firstName",
+              placeholder: "First Name",
+              id: "firstName",
+              autoComplete: "off",
+            }}
+            errors={errors}
+            register={register}
+          />
+        </div>
+        <div className="col-md-6 col-12 ">
+          <SimpleInput
+            validation={{
+              required: true,
+            }}
+            input={{
+              type: "text",
+              label: "Last Name",
+              name: "lastName",
+              placeholder: "Last Name",
+              id: "lastName",
+              autoComplete: "off",
+            }}
+            errors={errors}
+            register={register}
+          />
+        </div>
+      </div>
+      <div className="text-end">
+        <button className={`btn ${classes.submit}`} type="submit">
+          Continue
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default FirstStep;
