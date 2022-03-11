e40686dafbc1b9e2652a71ee4f84032595d49bd1
masoudvrz <masoud.varzi@yahoo.com>
2022-03-11 17:03:52 +0330

    add phone number and website input to second step

--- src/components/Steps/SecondStep/SecondStep.js
+++ src/components/Steps/SecondStep/SecondStep.js
@@ -1,5 +1,6 @@
 import classes from "./SecondStep.module.scss";
 import { useForm } from "react-hook-form";
+import SimpleInput from "../../SimpleInput/SimpleInput";
 
 const SecondStep = ({
   OnUpdateActiveStep,
@@ -25,8 +26,44 @@ const SecondStep = ({
       onSubmit={handleSubmit(onSubmit)}
     >
       <div className="row">
-        <div className=" col-12"></div>
-        <div className=" col-12 "></div>
+        <div className=" col-12">
+          <SimpleInput
+            validation={{
+              required: true,
+            }}
+            input={{
+              type: "text",
+              label: "Phone Number*",
+              name: "phoneNumber",
+              placeholder: "Phone Number",
+              id: "phoneNumber",
+              autoComplete: "off",
+            }}
+            errors={errors}
+            register={register}
+          />
+        </div>
+        <div className=" col-12 ">
+          <SimpleInput
+            validation={{
+              pattern: {
+                value:
+                  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
+                message: "Please enter a valid url",
+              },
+            }}
+            input={{
+              type: "text",
+              label: "Website",
+              name: "website",
+              placeholder: "Website",
+              id: "Website",
+              autoComplete: "off",
+            }}
+            errors={errors}
+            register={register}
+          />
+        </div>
         <div className=" col-12 "></div>
         <div className="col-12"></div>
         <div className="col-12"></div>
